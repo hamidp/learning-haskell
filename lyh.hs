2ca9f22c01252b88fe854247579e4193277ebb43
Hamid <hamid.palo@gmail.com>
2012-01-07 11:49:25 -0500

    Quicksort.

--- lyh.hs
+++ lyh.hs
@@ -17,11 +17,12 @@ calcBmis xs =
 	[bmi w h | (w, h) <- xs]  
     where bmi weight height = weight / height ^ 2  
 
-
+-- head
 head' :: [a] -> a  
 head' xs = case xs of [] -> error "No head for empty lists!"  
                       (x:_) -> x  
 
+-- maximum with accumulator
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "empty list"
 maximum' (x:[]) = x
@@ -31,3 +32,35 @@ maximum' (x:xs) =
         maxAcc (y:ys) a
             | y >= a = maxAcc ys y
             | otherwise = maxAcc ys a
+
+-- quicksort
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smaller = quicksort [ a | a <- xs, a <= x]
+      bigger  = quicksort [ a | a <- xs, a > x]
+  in smaller ++ [x] ++ bigger
+
+-- with partitioning function mostly following
+-- http://en.literateprograms.org/Quicksort_(Haskell)
+part :: Ord a => a -> [a] -> ([a],[a],[a]) -> ([a],[a],[a])
+--               e    lst     l    e   g       partitioned
+part _ [] (l,e,g) = (l,e,g)
+-- if the head of the list
+--   bigger: append go the greater list g
+--   smaller: append to the smaller list l
+--   equal: append to the equal list e 
+part p (x:xs) (l,e,g)
+  | x > p     = part p xs (l  , e  , x:g)
+  | x < p     = part p xs (x:l, e  , g)
+  | otherwise = part p xs (l  , x:e, g)
+
+qs2 :: Ord a => [a] -> [a]
+qs2 [] = []
+qs2 (x:xs) =
+  qs2 smaller ++ equal ++ qs2 bigger
+  where
+    (smaller, equal, bigger) = part x xs ([],[x],[])
+--                                        l   e   g
+
+
